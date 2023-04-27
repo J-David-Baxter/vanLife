79ad7efbdcc69458f84e46d9f62f099731b03f1c
J-David-Baxter <jdavidbx@gmail.com>
2023-04-27 18:49:32 -0400

    Create home and about routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { Box } from "@chakra-ui/react"
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Nav from "./components/Nav"
 
 
 function App() {
   return (
-    <Box>App</Box>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/"  element={<Home />}/>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import { Flex } from "@chakra-ui/react"
+
+
+const Nav = () => {
+  return (
+    <Flex>
+        Nav
+    </Flex>
+  )
+}
+
+export default Nav
