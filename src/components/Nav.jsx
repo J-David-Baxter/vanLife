087647fa326a771e6f40838c2de7975d618637c5
J-David-Baxter <jdavidbx@gmail.com>
2023-04-27 19:27:04 -0400

    Import font, style nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,10 +4,12 @@ import { Link as RouterLink } from "react-router-dom"
 
 const Nav = () => {
   return (
-    <Flex gap={5} align='center'>
-        <Heading>VanLife</Heading>
-        <Link as={RouterLink} to="/" >Home</Link>
-        <Link as={RouterLink} to="/about" >About</Link>
+    <Flex gap={10} align='center' backgroundColor='#FFF7ED' p={10} fontFamily='Inter'>
+        <Link as={RouterLink} to="/" mr='auto' _hover={{textDecoration: 'none'}}>
+            <Heading fontFamily='Inter'>#VANLIFE</Heading>
+        </Link>
+        <Link as={RouterLink} to="/about" fontSize={20} color='#4D4D4D' mr={5}>About</Link>
+        <Link as={RouterLink} to="/vans" fontSize={20} color='#4D4D4D' mr={5}>Vans</Link>
     </Flex>
   )
 }
