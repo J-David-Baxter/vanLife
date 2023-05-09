d4fa0cbeb3a3af9a8c945bca41ffda8bc85b9182
J-David-Baxter <jdavidbx@gmail.com>
2023-05-09 13:50:31 -0400

    Fix content alignment on van details page

--- src/pages/VanDetails.jsx
+++ src/pages/VanDetails.jsx
@@ -12,18 +12,18 @@ const VanDetails = () => {
  return (
     <>
       <Link to='/vans'><Text textDecoration='underline' cursor='pointer' ml={10}>Back to all vans</Text></Link>
-      <Stack w='50%' align='center' m='0 auto' gap={10} mb={10}>
+      <Stack w='40%' align='start' m='0 auto' gap={10} mb={10}>
         {loading && <Spinner color="#161616" size='xl'/>}
         {error && <Text>Something went wrong!</Text>}
         <Image 
           src={data && data.vans.imageUrl}
           alt={data && data.vans.name}
         />
-        <Stack alignSelf='start' gap={5}>
+        <Stack gap={5}>
           <VanTypeBadge type={data && data.vans.type} width={60}/>
           <Heading>{data && data.vans.name}</Heading>
           <Heading>${data && data.vans.price}/day</Heading>
-          <Text lineHeight={1.6} fontSize={18}>{data && data.vans.description}</Text>
+          <Text w='87%' lineHeight={1.6} fontSize={18}>{data && data.vans.description}</Text>
         </Stack>
       </Stack>
     </>
